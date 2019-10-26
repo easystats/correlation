7942b87dd83a8275c2449b1ed30eede18ebef951
Dominique Makowski <dom.mak19@gmail.com>
2019-10-26 23:15:22 +0800

    fix nonprinting of cormatrix if no attributes

--- R/print.easycormatrix.R
+++ R/print.easycormatrix.R
@@ -7,19 +7,24 @@ print.easycormatrix <- function(x, digits = 2, stars = TRUE, ...) {
   type <- names(p)[names(p) %in% c("BF", "pd", "p")][1]
   p <- p[[type]]
 
-  if (type == "p") {
-    p[, nums] <- sapply(p[, nums], parameters::format_p, stars_only = TRUE)
-  } else if (type == "pd") {
-    p[, nums] <- sapply(p[, nums], parameters::format_pd, stars_only = TRUE)
-  } else if (type == "BF") {
-    p[, nums] <- sapply(p[, nums], parameters::format_bf, stars_only = TRUE)
+  if (!is.null(p)) {
+    if (type == "p") {
+      p[, nums] <- sapply(p[, nums], parameters::format_p, stars_only = TRUE)
+    } else if (type == "pd") {
+      p[, nums] <- sapply(p[, nums], parameters::format_pd, stars_only = TRUE)
+    } else if (type == "BF") {
+      p[, nums] <- sapply(p[, nums], parameters::format_bf, stars_only = TRUE)
+    }
+
+    # Round and eventually add stars
+    x[, nums] <- sapply(as.data.frame(x)[, nums], insight::format_value, digits = digits)
+    if (stars) {
+      x[, nums] <- paste0(as.matrix(as.data.frame(x)[, nums]), as.matrix(p[, nums]))
+    }
+  } else{
+    x[, nums] <- sapply(as.data.frame(x)[, nums], insight::format_value, digits = digits)
   }
 
-  x[, nums] <- sapply(as.data.frame(x)[, nums], insight::format_value, digits = digits)
-
-  if (stars) {
-    x[, nums] <- paste0(as.matrix(as.data.frame(x)[, nums]), as.matrix(p[, nums]))
-  }
 
 
   cat(insight::format_table(x))
