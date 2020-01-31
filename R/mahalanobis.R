066fd594bd792dfe244165eab490e489a2f04247
Dominique Makowski <dom.mak19@gmail.com>
2020-01-31 17:23:08 +0800

    bootstrapped mahalanobis distance and CI
    close #30

--- R/mahalanobis.R
+++ R/mahalanobis.R
@@ -10,27 +10,32 @@
 #'   \item Schwarzkopf, D. S., De Haas, B., & Rees, G. (2012). Better ways to improve standards in brain-behavior correlation analysis. Frontiers in human neuroscience, 6, 200.
 #' }
 #'
+#' @examples
+#' distance_mahalanobis(data=iris[,1:4])
+#'
 #' @export
 distance_mahalanobis <- function(data, ci = 0.95, iterations=1000, robust=FALSE, ...) {
   if (robust) {
-    # rez <- boot::boot(data = data, statistic = .distance_mahalanobis, R = iterations, sim="permutation")
-    # bayestestR::point_estimate(as.data.frame(rez$t), centrality="all")
-    stop("not available yet.")
+    Ms <- matrix(data=NA, nrow=iterations, ncol=nrow(data))
+    for(i in 1:iterations){
+      # Draw random numbers from 1:n with replacement
+      x <- sample(1:nrow(data), nrow(data), replace = TRUE)
+      # Resample data
+      dat <- data[x,]
+      # Calculating the Mahalanobis distance for each actual observation using resampled data
+      m <- stats::mahalanobis(data, center = colMeans(dat), cov = stats::cov(dat))
+      Ms[i,] <- m
+    }
+    # Get summary
+    d <- bayestestR::describe_posterior(as.data.frame(Ms), centrality="median", ci=ci, test="pd")
+    d <- d[c("Median", "CI_low", "CI_high")]
+    names(d) <- c("Distance", "CI_low", "CI_high")
+
   } else {
-    d <- .distance_mahalanobis(data)
+    d <- stats::mahalanobis(data, center = colMeans(data), cov = stats::cov(data))
+    d <- data.frame(Distance = d)
     # TODO: get ci
     # stats::pchisq(p = 1 - ci, df = ncol(data))
   }
   d
-}
-
-
-
-
-
-#' @keywords internal
-.distance_mahalanobis <- function(data, indices = 1:nrow(data), ...) {
-  dat <- data[indices, ] # allows boot to select sample
-  row.names(dat) <- NULL
-  stats::mahalanobis(dat, center = colMeans(dat), cov = stats::cov(dat))
 }
