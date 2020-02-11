9e5aba9c1964811d3995f95d70b7c2a89bbef02b
Daniel <mail@danielluedecke.de>
2020-02-11 10:39:20 +0100

    namespace

--- R/cor_test_percentage.R
+++ R/cor_test_percentage.R
@@ -1,11 +1,12 @@
+#' @importFrom stats pt median
 #' @keywords internal
 .cor_test_percentage <- function(data, x, y, ci = 0.95, beta = 0.2, ...) {
   var_x <- .complete_variable_x(data, x, y)
   var_y <- .complete_variable_y(data, x, y)
 
-  temp <- sort(abs(var_x - median(var_x)))
+  temp <- sort(abs(var_x - stats::median(var_x)))
   omhatx <- temp[floor((1 - beta) * length(var_x))]
-  temp <- sort(abs(var_y - median(var_y)))
+  temp <- sort(abs(var_y - stats::median(var_y)))
   omhaty <- temp[floor((1 - beta) * length(var_y))]
   a <- (var_x - .pbos(var_x, beta)) / omhatx
   b <- (var_y - .pbos(var_y, beta)) / omhaty
@@ -17,7 +18,7 @@
   # Result
   r <- sum(a * b) / sqrt(sum(a^2) * sum(b^2))
   test <- r * sqrt((length(var_x) - 2) / (1 - r^2))
-  sig <- 2 * (1 - pt(abs(test), length(var_x) - 2))
+  sig <- 2 * (1 - stats::pt(abs(test), length(var_x) - 2))
   ci_vals <- cor_to_ci(r, n = length(var_x), ci = ci)
 
   data.frame(
@@ -39,9 +40,9 @@
 
 #' @keywords internal
 .pbos <- function(x, beta = 0.2) {
-  temp <- sort(abs(x - median(x)))
+  temp <- sort(abs(x - stats::median(x)))
   omhatx <- temp[floor((1 - beta) * length(x))]
-  psi <- (x - median(x)) / omhatx
+  psi <- (x - stats::median(x)) / omhatx
   i1 <- length(psi[psi < (-1)])
   i2 <- length(psi[psi > 1])
   sx <- ifelse(psi < (-1), 0, x)
