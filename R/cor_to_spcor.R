b97975de6790c5b0488356834f8cae142e42e4bc
DominiqueMakowski <dom.mak19@gmail.com>
2020-02-26 10:47:26 +0800

    update

--- R/cor_to_spcor.R
+++ R/cor_to_spcor.R
@@ -11,7 +11,7 @@ cor_to_spcor <- function(cor = NULL, cov = NULL, tol = .Machine$double.eps^(2 / 
     stop("Covariance matrix (or vector of SD of variables) needs to be passed for semi-partial correlations.")
   } else {
     if (!is.matrix(cov)) {
-      cov <- cor2cov(cor, sd = cov)
+      cov <- cor_to_cov(cor, sd = cov)
     }
     inverted <- .invert_matrix(cov, tol = tol)
     out <- -cov2cor(inverted) / sqrt(diag(cov)) / sqrt(abs(diag(inverted) - t(t(inverted^2) / diag(inverted))))
@@ -46,7 +46,7 @@ spcor_to_cor <- function(spcor = NULL, cov = NULL, semi = FALSE, tol = .Machine$
   #   stop("Covariance matrix (or vector of SD of variables) needs to be passed for semi-partial correlations.")
   # } else{
   #   if(!is.matrix(cov)){
-  #     cov <- cor2cov(spcor, sd = cov)
+  #     cov <- cor_to_cov(spcor, sd = cov)
   #   }
   #   inverted <- inverted * sqrt(diag(cov)) * sqrt(abs(diag(inverted) - t(t(inverted^2) / diag(inverted))))
   # }
