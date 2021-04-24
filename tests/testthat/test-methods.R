51dc70d3f30b0688277c8db8372605dc1c9c3ee0
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2021-04-24 09:34:54 -0400

    Add error check to summary target_col test

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -8,4 +8,5 @@ test_that("summary.correlation - target column", {
   expect_snapshot(summary(correlation(ggplot2::msleep), target_col = "t"))
   expect_snapshot(summary(correlation(ggplot2::msleep), target_col = "df_error"))
   expect_snapshot(summary(correlation(ggplot2::msleep), target_col = "p"))
+  expect_error(summary(correlation(ggplot2::msleep), target_col = "not_a_column_name"))
 })
