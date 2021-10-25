ff9995edea4889b73b8c82be2859b4fa648b96e5
Dominique Makowski <dom.mak19@gmail.com>
2021-10-25 22:59:30 +0800

    plotting fixed

--- R/methods_plot.R
+++ R/methods_plot.R
@@ -1,14 +1,12 @@
 #' @export
-plot.easycormatrix <- function(x, ...) {
-  insight::check_if_installed("see", "to plot correlation graphs")
-
-  NextMethod()
-}
-
-
-#' @export
-plot.easycor_test <- function(x, ...) {
+plot.easycorrelation <- function(x, ...) {
   insight::check_if_installed("see", "to plot correlation graphs")
 
   plot(visualisation_recipe(x, ...), ...)
 }
+
+#' @export
+plot.easycormatrix <- plot.easycorrelation
+
+#' @export
+plot.easycor_test <- plot.easycorrelation
---
+++ R/visualisation_recipe.easycorrelation.R
@@ -0,0 +1,41 @@
+#' @rdname visualisation_recipe.easycormatrix
+#'
+#' @examples
+#' # ==============================================
+#' # Correlation Results (easycorrelation)
+#' # ==============================================
+#' if (require("see")) {
+#' rez <- correlation(iris)
+#'
+#' layers <- visualisation_recipe(rez)
+#' layers
+#' plot(layers)
+#' }
+#'
+#' @export
+visualisation_recipe.easycorrelation <- function(x, ...) {
+  insight::check_if_installed("tidygraph")
+
+  x$width <- abs(x$r)
+  data <- tidygraph::as_tbl_graph(x)
+
+  # Initialize layers list
+  layers <- list()
+
+  layers[["l1"]] <- list(geom = "ggraph::geom_edge_arc",
+                         strength = 0.1,
+                         aes = list(edge_colour = "r", edge_width = "width"))
+  layers[["l2"]] <- list(geom = "ggraph::geom_node_point", size = 22)
+  layers[["l3"]] <- list(geom = "ggraph::geom_node_text",
+                         aes = list(label = "name"))
+  layers[["l4"]] <- list(geom = "ggraph::theme_graph")
+  layers[["l5"]] <- list(geom = "guides", edge_width = "none")
+
+  # Out
+  class(layers) <- c("visualisation_recipe", "see_visualisation_recipe", class(layers))
+  attr(layers, "data") <- data
+  attr(layers, "layout") <- "kk"
+  attr(layers, "ggraph") <- TRUE
+  layers
+
+}
