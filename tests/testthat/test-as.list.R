6ef7072194d88832a80278f194e8bb797c8fed21
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-11-05 15:26:11 +0100

    suppress warnings in test

--- tests/testthat/test-as.list.R
+++ tests/testthat/test-as.list.R
@@ -7,14 +7,16 @@ if (require("poorman", quietly = TRUE) && require("ggplot2", quietly = TRUE)) {
 
     # with groups
     set.seed(123)
-    expect_snapshot(as.list(correlation(group_by(msleep, vore), method = "spearman")))
+    expect_snapshot(suppressWarnings(as.list(msleep %>%
+      group_by(vore) %>%
+      correlation(method = "spearman"))))
 
-    expect_snapshot(as.list(mtcars %>%
+    expect_snapshot(suppressWarnings(as.list(mtcars %>%
       group_by(am) %>%
       correlation(
         select = c("cyl", "wt"),
         select2 = c("hp"),
         method = "percentage"
-      )))
+      ))))
   })
 }
