df0e0b07b95973eb5c8f76c5a1e894d11669b30f
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-20 11:24:36 +0200

    Reexport standardize names
    Closes #255

--- R/reexports.R
+++ R/reexports.R
@@ -6,6 +6,10 @@ bayestestR::simulate_simpson
 #' @importFrom datawizard visualisation_recipe
 datawizard::visualisation_recipe
 
+#' @export
+#' @importFrom parameters standardize_names
+parameters::standardize_names
+
 #' @importFrom insight print_md
 #' @export
 insight::print_md
