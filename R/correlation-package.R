efe788053fb42d41c7d17256e4325995f94d4067
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2022-10-12 14:31:51 -0400

    Add package description help file (#272)

---
+++ R/correlation-package.R
@@ -0,0 +1,18 @@
+#' \code{correlation}
+#'
+#' @title correlation: Methods for correlation analysis
+#'
+#' @description
+#'
+#' Lightweight package for computing different kinds of correlations,
+#' such as partial correlations, Bayesian correlations, multilevel correlations,
+#' polychoric correlations, biweight correlations, distance correlations and more.
+#' Part of the 'easystats' ecosystem.
+#'
+#' References: Makowski et al. (2020) <doi:10.21105/joss.02306>.
+#'
+#' @docType package
+#' @aliases correlation-package
+#' @name correlation-package
+#' @keywords internal
+"_PACKAGE"
