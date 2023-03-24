55fc469e5c7f5b4d216753c38a13935e2f3c565e
etiennebacher <etienne.bacher@protonmail.com>
2023-03-24 08:53:54 +0100

    missing "MASS" in one of the tests

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -22,6 +22,7 @@ test_that("z_fisher works", {
 })
 
 test_that("simulate_simpson works", {
+  skip_if_not_or_load_if_installed("MASS")
   set.seed(123)
   df <- bayestestR::simulate_simpson(n = 100, groups = 5, r = 0.5)
   expect_equal(dim(df), c(500L, 3L))
