fde655c39d67bc980363b4c0432562209c64350d
Dominique Makowski <dom.mak19@gmail.com>
2024-12-18 08:25:09 +0000

    minor

--- tests/testthat/test-cor_sort.R
+++ tests/testthat/test-cor_sort.R
@@ -1,17 +1,34 @@
 test_that("cor_sort", {
-  r <- cor(mtcars)
-  expect_equal(as.numeric(diag(r)), rep(1, ncol(mtcars)))
-  # heatmap(r, Rowv = NA, Colv = NA)
 
-  r1 <- cor_sort(r)
-  expect_equal(as.numeric(diag(r)), rep(1, ncol(mtcars)))
-  # heatmap(r1, Rowv = NA, Colv = NA)
+  # Basic -------------------------------------------------------------------
 
+  # Square
+  r1 <- cor(mtcars)
+  expect_equal(as.numeric(diag(r1)), rep(1, ncol(mtcars)))
+  # heatmap(r1, Rowv = NA, Colv = NA)  # visualize
+
+  r1sort <- cor_sort(r1)
+  expect_equal(as.numeric(diag(r1sort)), rep(1, ncol(mtcars)))
+  # heatmap(r1sort, Rowv = NA, Colv = NA)  # visualize
+
+  # Non-square
   r2 <- cor(mtcars[names(mtcars)[1:5]], mtcars[names(mtcars)[6:11]])
   expect_equal(rownames(r2), names(mtcars)[1:5])
-  # heatmap(r2, Rowv = NA, Colv = NA)
+  # heatmap(r2, Rowv = NA, Colv = NA)  # visualize
 
-  r3 <- cor_sort(r2)
-  expect_equal(all(rownames(r3) == names(mtcars)[1:5]), FALSE)
-  # heatmap(r3, Rowv = NA, Colv = NA)
+  r2sort <- cor_sort(r2)
+  expect_equal(all(rownames(r2sort) == names(mtcars)[1:5]), FALSE)
+  # heatmap(r2sort, Rowv = NA, Colv = NA)  # visualize
+
+  # correlation() -----------------------------------------------------------
+  # TODO.
+  rez1 <- correlation::correlation(mtcars)
+  # rez1sort <- cor_sort(rez1)
+
+  # summary(correlation()) --------------------------------------------------
+  # TODO.
+
+  # as.matrix(correlation()) ------------------------------------------------
+  # TODO.
+  # rez1m <- as.matrix(rez1sort)
 })
